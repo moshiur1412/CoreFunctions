5d10eebea7ff82ab81c09fa560ffcf0314f5cd7e
vorsurm <moshiur1412@gmail.com>
2020-02-22 01:10:56 +0600

    Added Questions & Users information

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,16 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Str;
+
 
 class Question extends Model
 {
+    protected $fillable = ['title', 'body'];
+
     public function user(){
-        return $this->belogsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function setTitleAttribute($value){
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
+        $this->attributes['slug'] = str_slug($value);
+    }
+    
+    public function getUrlAttribute(){
+        return route('questions.show', $this->id);
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
     }
 }
