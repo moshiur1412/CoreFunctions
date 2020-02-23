18958c9bc6cea2a2630d472b45db8c62d0e9eeb6
vorsurm <moshiur1412@gmail.com>
2020-02-24 00:16:42 +0600

    Generated Dynamic Answer Count

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -6,11 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    public function questions(){
-        return $this->hasMany(Question::class);
+    public function question(){
+        return $this->belongsTo(Question::class);
     }
 
-    public function users(){
-        return $this->hasMany(User::class);
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public static function boot(){
+    
+        parent::boot();
+
+        static::created(function($answer){
+            
+            echo "Created done\n";
+            $answer->question->increment('answers_count');
+            $answer->save();
+           
+       });
+
+        static::saved(function(){
+            echo "Saved done\n";
+       });
+
+
     }
 }
