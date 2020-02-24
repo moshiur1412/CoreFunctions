53e5a13101731f9e6bcc0e0e69e0c18c03e71d88
vorsurm <moshiur1412@gmail.com>
2020-02-24 17:14:31 +0600

    Vote Counter UI Design almost done

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('content')
 
 <div class="container">
@@ -6,35 +7,58 @@
         <div class="col-md-12">
             <div class="card">
 
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h1> {{ $question->title }}</h1>
-                        <div class="ml-auto">
-                            <a href="{{route('questions.index')}}" class="btn btn-outline-secondary"> Back to All Questions </a>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="card-body">
-                    {!! $question->body_html !!}
-
-
-                    <div class="float-right">
-                        <span class="text-muted">Questioned {{ $question->created_date }}</span>
-                        <div class="media mt-2">
-                            <a href="{{$question->user->url}}">
-                            <img src="{{$question->user->gravatar }}" alt="{{$question->user->name}}">
-                            </a>
-
-                            <div class="media-body m-1">
-                                <a href="{{ $question->user->url }}"> {{ $question->user->name }} </a>
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h1> {{ $question->title }}</h1>
+                            <div class="ml-auto">
+                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary"> Back to All Questions </a>
                             </div>
                         </div>
                     </div>
-                </div>
-        </div>
-        </div>
-    </div>
+                    <hr>
+
+                    <div class="media">
+
+                        <div class="d-fex flex-column vote-controls">
+
+                            <a href="" title="This question is useful" class="vote-up">
+                                <i class="fas fa-caret-up fa-3x"> </i>
+                            </a>
+
+                            <span class="votes-count"> 1024 </span>
+
+                            <a href="" title="This question is not useful" class="vote-down off">
+                                <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+
+                            <a href="" title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-2x"></i>
+                                <span class="favorites-count"> 1254 </span>
+                             </a>
+
+                        </div>
+
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Questioned {{ $question->created_date }}</span>
+                                <div class="media mt-2">
+                                    <a href="{{$question->user->url}}">
+                                    <img src="{{$question->user->gravatar }}" alt="{{$question->user->name}}">
+                                    </a>
+                                    <div class="media-body m-1">
+                                        <a href="{{ $question->user->url }}"> {{ $question->user->name }} </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div> <!--/* end media-body class -->
+                    </div> <!--\* end media class -->
+                </div><!--\* end card-body class -->
+            </div><!--\* end card class -->
+        </div><!--\* end col-md -->
+    </div><!--\* end row class -->
 
     <div class="row mt-5">
         <div class="col-md-12">
