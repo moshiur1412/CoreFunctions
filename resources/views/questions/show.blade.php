ab11c715739875c1fef6b7d633f6e58406581ee5
vorsurm <moshiur1412@gmail.com>
2020-04-08 21:13:06 +0600

    First implement vue.js for author page

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -34,10 +34,9 @@
                                 <div class="col-4"></div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                @include('shared._author', [
-                                    'label' => 'Asked',
-                                    'model' => $question
-                                ])
+                              
+                                    <user-info :model="{{ $question }}" label="Asked"></user-info>
+                              
                                 </div>
                             </div>
                         </div> <!--/* end media-body class -->
