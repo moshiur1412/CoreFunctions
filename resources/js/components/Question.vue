e5d0ed9e1195e791a798b1ac6ef647ea114dd48f
vorsurm <moshiur1412@gmail.com>
2020-04-28 16:35:15 +0600

    Creating Question Component

---
+++ resources/js/components/Question.vue
@@ -0,0 +1,50 @@
+<template>
+     <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+
+                <div class="card-body">
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h1> {{ title }}</h1>
+                            <div class="ml-auto">
+                                <a href="/questions" class="btn btn-outline-secondary"> Back to All Questions </a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="media">
+                        <div class="d-fex flex-column vote-controls">    
+                           <vote :model="question" :name="'question'"></vote>                           
+                        </div>
+
+                        <div class="media-body">
+                            <div v-html="bodyHtml"></div>
+                            <div class="row">
+                                <div class="col-4"></div>
+                                <div class="col-4"></div>
+                                <div class="col-4">
+                                    <user-info :model="question" label="Asked"></user-info>
+                                </div>
+                            </div>
+                        </div> <!--/* end media-body class -->
+                    </div> <!--\* end media class -->
+                </div><!--\* end card-body class -->
+            </div><!--\* end card class -->
+        </div><!--\* end col-md -->
+    </div><!--\* end row class -->
+</template>
+
+<script>
+export default {
+    props: ['question'],
+    data(){
+        return{
+            title: this.question.title,
+            body: this.question.body,
+            bodyHtml: this.question.body_html
+        }
+    }
+}
+</script>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,62 +3,9 @@
 @section('content')
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-
-                <div class="card-body">
-                    <div class="card-title">
-                        <div class="d-flex align-items-center">
-                            <h1> {{ $question->title }}</h1>
-                            <div class="ml-auto">
-                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary"> Back to All Questions </a>
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
-
-                    <div class="media">
-
-                        <div class="d-fex flex-column vote-controls">
-{{--                             
-                            @include('shared._vote', [
-                                'model' => $question,
-                                'model_name' => 'Question'
-                            ]) --}}
-
-                            <vote :model="{{$question}}" :name="'question'"></vote>
-                           
-                        </div>
-
-                        <div class="media-body">
-                            {!! $question->body_html !!}
-                            <div class="row">
-                                <div class="col-4"></div>
-                                <div class="col-4"></div>
-                                <div class="col-4">
-                                    <user-info :model="{{ $question }}" label="Asked"></user-info>
-                                </div>
-                            </div>
-                        </div> <!--/* end media-body class -->
-                    </div> <!--\* end media class -->
-                </div><!--\* end card-body class -->
-            </div><!--\* end card class -->
-        </div><!--\* end col-md -->
-    </div><!--\* end row class -->
-
-
-{{-- <answers :answers="{{$question->answers }}" :count="{{$question->answers_count}}"></answers> --}}
+<question :question="{{$question}}"></question>
 <answers :question="{{ $question }}"></answers>
 
-    {{-- @include('answers._index', [
-       'answersCount' =>  $question->answers_count,
-       'answers' => $question->answers,
-    ]) --}}
-
-    {{-- @include('answers._create') --}}
-
-</div>
 </div>
 
 @endsection
