41166d8fbbf6b3ab876d5bf5dee7e673560c8949
vorsurm <moshiur1412@gmail.com>
2020-05-02 20:02:42 +0600

    Implemented jwt API authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Question;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -110,5 +112,25 @@ class User extends Authenticatable
         return $model->votes_count;
     }
 
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     
 }
