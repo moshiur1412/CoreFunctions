265d1515085510fac333295ead0c78ad831e3c20
vorsurm <moshiur1412@gmail.com>
2020-05-03 20:42:48 +0600

    Getting the topic and post data together

--- app/Http/Resources/Topic.php
+++ app/Http/Resources/Topic.php
@@ -3,17 +3,22 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Post as PostResources;
 
 class Topic extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+
     public function toArray($request)
     {
-        return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'created_at' => $this->created_at->diffForHumans(),
+            'updated_at' => $this->updated_at->diffForHumans(),
+            'post' => PostResources::collection($this->posts),
+            'user' => $this->user
+
+        ];
     }
 }
