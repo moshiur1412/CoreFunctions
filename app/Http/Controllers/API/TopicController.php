0d5d151cc7775bdd8fe76ac0d74e707242707632
vorsurm <moshiur1412@gmail.com>
2020-05-04 19:26:32 +0600

    Post Validation Added

--- app/Http/Controllers/API/TopicController.php
+++ app/Http/Controllers/API/TopicController.php
@@ -4,23 +4,24 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 
-use Illuminate\Http\Request;
-
+// use Illuminate\Http\Request;
+use App\Http\Requests\StorePost;
 use App\Models\Topic;
 use App\Models\Post;
+
 use App\Http\Resources\Topic as TopicResource;
 
 class TopicController extends Controller
 {
-    public function store(Topic $topic, Post $post){
+    public function store(StorePost $storePost, Topic $topic, Post $post){
 
         \Log::info("Req=API\TopicController@store called");
 
-        $topic->title = request()->title;
-        $topic->user()->associate(request()->user());
+        $topic->title = $storePost->title;
+        $topic->user()->associate($storePost->user());
 
-        $post->body = request()->body;
-        $post->user()->associate(request()->user());
+        $post->body = $storePost->body;
+        $post->user()->associate($storePost->user());
 
         $topic->save();
         $topic->posts()->save($post);
