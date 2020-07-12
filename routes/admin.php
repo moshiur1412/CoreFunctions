e1ca578e5eb1d7bb002fc55a53a25642a78c403c
vorsurm <moshiur1412@gmail.com>
2020-07-12 11:57:09 +0600

    Fixed custom routes

--- routes/admin.php
+++ routes/admin.php
@@ -3,3 +3,7 @@
 Route::get('/', function(){
 	dd('worked from admin');
 });
+
+Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+	Route::get('/dashboard', 'AdminController@index')->name('admin.home');
