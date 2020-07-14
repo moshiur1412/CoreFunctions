1b997b7e253d4285fac05622e2499831b6f69659
vorsurm <moshiur1412@gmail.com>
2020-07-14 12:28:31 +0600

    Browser login test done

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+
+class LoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * @group login-test
+     */
+    public function testAUserCanLogin(){
+
+        $user = factory(User::class)->create();
+
+        $this->browse(function(Browser $browser) use($user){
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'password')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+    }
+}
